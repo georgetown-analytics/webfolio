d7f7be7b0eb3797e5d38256605fe088e9c1e5aec
Benjamin Bengfort <benjamin@bengfort.com>
2020-01-03 11:56:17 -0500

    version bump to 0.3

--- webfolio/version.py
+++ webfolio/version.py
@@ -26,10 +26,10 @@ SLUG_COMMIT_ENV = [
 
 __version_info__ = {
     'major': 0,
-    'minor': 2,
+    'minor': 3,
     'micro': 0,
     'releaselevel': 'final',
-    'serial': 2,
+    'serial': 3,
 }
 
 
